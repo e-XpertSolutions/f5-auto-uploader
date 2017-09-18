735b42dffb8cf3a7a39a16936ecccfaff3b61be2
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2017-09-18 14:44:37 +0200

    test: skip scanDir test

--- scan_test.go
+++ scan_test.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"testing"
-
-	"github.com/e-XpertSolutions/f5-rest-client/f5"
 )
 
 func TestScanDir(t *testing.T) {
-	f5Client, _ := f5.NewBasicClient("https://192.168.10.40", "admin", "admin")
-	f5Client.DisableCertCheck()
-	if err := scanDir("/tmp/test", f5Client); err != nil {
-		t.Error(err)
-	}
+	t.Skip()
 }
